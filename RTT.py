d72f8a36fabe2013a5efae0941ed9b217721ba7c
Diego CQI <diego.torreblanca77@gmail.com>
2023-05-23 22:38:41 -0400

    update RTT.py now works

--- RTT.py
+++ RTT.py
@@ -1,30 +1,62 @@
 from scapy.all import *
-from datetime import datetime
+import csv
+import locale
+import matplotlib.pyplot as plt
 
-pcap_file=("out/cqi_4_ms/mypcapfile-5-1.pcap")
-ti = datetime.now()
+def get_RTT(pcap_filename):
 
+    # Read the pcap file
+    packets = rdpcap(pcap_filename)
 
-# Read the pcap file
-packets = rdpcap(pcap_file)
+    # Dictionary to store packet timestamps
 
+    packet_timestamps = {}
+    rtt_dict = {}
 
-# Dictionary to store packet timestamps
-packet_timestamps = {}
+    # Iterate over each packet
+    for packet in packets:
+        if packet.haslayer(TCP):
+            seq_num = packet[TCP].seq
+            ack_num = packet[TCP].ack
+            timestamp = packet.time
+            TCP_payload = len(packet[TCP].payload)
 
-# Iterate over each packet
-for packet in packets:
-    if packet.haslayer(TCP):
-        seq_num = packet[TCP].seq
-        ack_num = packet[TCP].ack
-        timestamp = packet.time
-        if seq_num not in packet_timestamps:
-            packet_timestamps[seq_num] = timestamp
-        else:
-            rtt = timestamp - packet_timestamps[seq_num]
-            print(f"Sequence Number: {seq_num}, Acknowledgment Number: {ack_num}, RTT: {rtt} seconds")
+            if seq_num == 0:
+                continue
 
-tf = datetime.now()
-print(" ")
-print(tf-ti)
+            if seq_num == 1 and ack_num == 1 and TCP_payload == 0:
+                continue
 
+            if seq_num not in packet_timestamps:
+                packet_timestamps[seq_num] = timestamp
+                rtt_dict[seq_num] = 0
+
+            recovered_seq_num = ack_num-1448
+            
+            if (seq_num == 1) and  (recovered_seq_num in packet_timestamps):
+                rtt = timestamp - packet_timestamps[recovered_seq_num]
+                rtt_dict[recovered_seq_num] = rtt*1000
+
+    # for seq in rtt_dict:
+    #     n_packet = int((seq-1)/1448 + 1)
+    #     if rtt_dict[seq] > 0:
+    #         print(f"El paquete N°{n_packet} de Sequence Number: {seq}, RTT: {rtt_dict[seq]} milliseconds")
+    #     else:
+    #         print(f"El paquete N°{n_packet} de Sequence Number: {seq} se perdió :(")
+
+    # File path to save the CSV
+    file_path = 'RTT.csv'
+
+    # Open the CSV file in write mode
+    with open(file_path, 'w', newline='') as file:
+        # Create a CSV writer
+        writer = csv.writer(file, delimiter=';')
+
+        # Write the header
+        writer.writerow(["Sequence number", "RTT [ms]"])
+
+        # Write data rows
+        for seq, rtt in rtt_dict.items():
+            writer.writerow([seq, rtt])
+
+    print("CSV file has been written successfully.")
