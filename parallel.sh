dc39e94db42a3a4c2a88791b3c49237ee8f39878
Agustin G <agustin.gonzalez.u@gmail.com>
2023-07-09 22:43:36 -0400

    Add: flag to set activate a different RngRun
    The flag is for randomizing and the run is chosen as the simulation number, so it could still be deterministic if we run the same number of simulations more than once (I dont see why we would do that though)

--- parallel.sh
+++ parallel.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # TODO: Actualmente paraleliza por tandas, hacer que pueda ir uno por uno (asignando una sim a 1 core)
+# TODO: Implementar método para poder cancelar/matar a los subprocesos
+# TODO: Revisar (y actualizar de ser necesario) para que sea compatible con linux y macos
 # INFO: Ahora mismo solo corre numero_de_cores - 1 simulaciones
 #       HAY que buildear antes de llamar, ya que no buildea ns3.
 
@@ -19,21 +21,24 @@ outpath==""
 sim_num=1
 nparam=0
 custom_name=""
+random=0
 
 helpFunction()
 {
    echo ""
-   echo "Usage: $0 -m $montecarlo -c $custom_name"
+   echo "Usage: $0 -m $montecarlo -c $custom_name -r"
    echo -e "\t-m Number of montecarlo iterations"
    echo -e "\t-c Custom folder name for the simulation data, the number of parameter will be prefixed"
+   echo -e "\t-r Flag that sets that a random run is used so results are not deterministic"
    exit 1 # Exit script after printing help
 }
 
-while getopts "m:c:" opt
+while getopts "m:c:r" opt
 do
    case "$opt" in
       m ) montecarlo="$OPTARG" ;;
       c ) custom_name="$OPTARG" ;;
+      r ) random=1 ;;
       ? ) helpFunction ;; # Print helpFunction in case parameter is non-existent
    esac
 done
@@ -60,8 +65,14 @@ for param in "${parameters[@]}"; do
 
         rem=$((sim_num % num_cores))
 
-        bash "cqi-probe.sh" -b -c "$outdir/SIM${mont_num}" -p "`echo $param`" > "$RUTA_PROBE/out/$outdir/outputs/sim${mont_num}.txt" && echo "Done $sim_num "&
-        printf "[sim:${blue}${sim_num}${clear} pid:${cyan}$!${clear}] Called ${green}cqi-probe.sh -c \"SIM${mont_num}\" -p ${param} ${clear}\n"
+        param2=$param;
+        if [ "$random" == "1" ]; then
+            param2="$param --RngRun=$sim_num"
+        fi
+
+        stdoutTxt=$RUTA_PROBE/out/$outdir/outputs/sim${mont_num}.txt
+        bash "cqi-probe.sh" -b -c "$outdir/SIM${mont_num}" -p "`echo $param2`" > "$stdoutTxt" && printf "Done $sim_num ${red}-${clear} Exit Status $?"&
+        printf "[sim:${blue}${sim_num}${clear} pid:${cyan}$!${clear}] Called ${green}cqi-probe.sh -b -c \"SIM${mont_num}\" -p ${param2} ${clear}\n"
 
         if [ "$rem" == "$((num_cores-1))" ]; then
             jobs
