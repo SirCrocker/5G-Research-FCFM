2a18af86a880ee3c9026cb793ddfb8416e6a99d4
Agustin G <agustin.gonzalez.u@gmail.com>
2023-07-24 21:30:07 -0400

    Update: uppercased variables and added a prefix
    Added 'TXT_' before each variable and uppercased them so we can identify in other code that they are constants

--- sim/cmdline-colors.h
+++ sim/cmdline-colors.h
@@ -2,11 +2,11 @@
 
 namespace ns3
 {
-    const std::string clear = "\033[0m" ;       // Remove color (plain text)
-    const std::string red = "\033[0;31m" ;      // Red text color
-    const std::string green = "\033[0;32m" ;    // Green text color
-    const std::string yellow = "\033[0;33m" ;   // Yellow text color
-    const std::string blue = "\033[0;34m" ;     // Blue text color
-    const std::string magenta = "\033[0;35m" ;  // Magenta text color
-    const std::string cyan = "\033[0;36m" ;     // Cyan text color
+    const std::string TXT_CLEAR = "\033[0m" ;       // Remove color (plain text)
+    const std::string TXT_RED = "\033[0;31m" ;      // Red text color
+    const std::string TXT_GREEN = "\033[0;32m" ;    // Green text color
+    const std::string TXT_YELLOW = "\033[0;33m" ;   // Yellow text color
+    const std::string TXT_BLUE = "\033[0;34m" ;     // Blue text color
+    const std::string TXT_MAGENTA = "\033[0;35m" ;  // Magenta text color
+    const std::string TXT_CYAN = "\033[0;36m" ;     // Cyan text color
 } // namespace ns3
